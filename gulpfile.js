89ea7522eddc66c5cba2540191cb935bc80d95e1
Anton Z <ztosh@uk.net>
2017-10-15 18:33:57 +0300

    gulp first commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+"use strict";
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    useref = require('gulp-useref'),
+    gulpIf = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    csso = require('gulp-csso'),
+    prefix = require('gulp-autoprefixer');
+
+gulp.task('server', function() {
+    browserSync.init({
+      server: {
+        port: 3000,
+        baseDir: "./app"
+      }
+    });
+});
+
+gulp.task('styles', function(){
+  gulp.src('./app/sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefix({
+      browsers: ['last 15 versions']
+    }))
+    .pipe( gulp.dest('./app/css') )
+    .pipe(browserSync.stream());
+});
+
+gulp.task('build', function(){
+  gulp.src('./app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', csso()))
+    .pipe(gulp.dest('./dist'))
+})
+
+
+gulp.task('watch', function(){
+  gulp.watch('./app/sass/**/*.sass', ['styles']);
+});
+
+gulp.task('default', ['watch', 'server']);
