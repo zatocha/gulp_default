9594ef0c251e6c9b93ac0559a0374e53cd4cdcaa
zatocha <ztosha@gmail.com>
2017-12-29 12:35:37 +0200

    new plugins

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,10 @@ var gulp = require('gulp'), // Подключаем Gulp
 	spritesmith = require('gulp.spritesmith'), // Для создания CSS спрайтов
 	imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
 	pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-	cache = require('gulp-cache'), // Подключаем библиотеку кеширования
 	includer = require("gulp-x-includer"), // Для вставки шаблонов
-	del = require('del'); // Для удаления файлов и папок
+	del = require('del'), // Для удаления файлов и папок
+	rename = require("gulp-rename"), // Для перименования файлов
+	noopener = require('gulp-noopener'); // Защита внешних ссылок
 
 gulp.task('server', function () { // Создаем task browser-sync
 	browserSync.init({
@@ -32,6 +33,16 @@ gulp.task('sass', function () {
 		}))
 		.pipe(gulp.dest('./app/css')) // Выгружаем результат в папку app/css
 		.pipe(browserSync.stream()) // Обновляем CSS на странице при изменении
+});
+
+gulp.task('fileInclude', function () {
+	gulp.src('./app/_*.html')
+		.pipe(includer()) // выполняем include html файлов
+		.pipe(rename(function (path) {
+			path.basename = path.basename.replace(/^_/, '');
+		}))
+		.pipe(gulp.dest('./app'))
+		.pipe(browserSync.stream())
 });
 
 gulp.task('sprite', function () {
@@ -58,23 +69,25 @@ gulp.task('clean', function () {
 	return del.sync('./build') // Удаляем папку dist перед сборкой
 });
 
-gulp.task('build', ['clean', 'sass', 'img'], function () { // создаем task для сборки проекта
-	gulp.src('./app/*.html') // Берем источник
+gulp.task('build', ['clean', 'img', 'sass'], function () { // создаем task для сборки проекта
+	gulp.src(['./app/_*.html', '!./app/-*.html']) // Берем источник
 		.pipe(includer()) // выполняем include html файлов
+		.pipe(rename(function (path) {
+			path.basename = path.basename.replace(/^_/, '');
+		}))
 		.pipe(useref()) // конкатенируем скрипты и стили
 		.pipe(gulpIf('*.js', uglify())) // сжимаем js
 		.pipe(gulpIf('*.css', csso())) // минифицируем стили
+		.pipe(noopener.overwrite()) // Добавляем защиту ссылок
 		.pipe(gulp.dest('./build')); // выгружаем скрипты и js в папку build
 	gulp.src('./app/fonts/**/*') // Берем источник
 		.pipe(gulp.dest('./build/fonts')); // Выгружаем шрифты в папку build
 });
 
-
 gulp.task('watch', function () { // создаем task для отслеживания изменений в sass
-	gulp.watch('./app/sass/**/*.sass', ['sass']); // Наблюдение за SASS файлами в папке sass
-	gulp.watch('./app/sass/**/*.scss', ['sass']); // Наблюдение за SCSS файлами в папке sass
-	gulp.watch('./app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(['./app/sass/**/*.sass', './app/sass/**/*.scss'], ['sass']); // Наблюдение за SASS и SCSS файлами
+	gulp.watch(['./app/-*.html', './app/_*.html'], ['fileInclude']); // Наблюдение за HTML файлами в корне проекта
 	gulp.watch('./app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
-gulp.task('default', ['watch', 'server']);
+gulp.task('default', ['watch', 'fileInclude', 'server']);
